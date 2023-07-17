9b5e0238863ff9ada230f749ec65dc244eff0813
Hindeu <whisper.hind@gmail.com>
2023-07-17 16:53:37 -0700

    If Else While commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print the alphabet in reverse
+ *        followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char L;
+
+        for (L = 'z'; L <= 'a'; L--)
+                putchar(L);
+        putchar('\n');
+        return (0);
+}
