b231caf32d6b7bd306e3ee25dbfea06aea45834b
Hindeu <whisper.hind@gmail.com>
2023-07-18 16:24:20 -0700

    Nested loops commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * print_alphabet :  print the alphabet,
+ *                   in lowercase,
+ *                   followed by a new line.
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	char L;
