f91a5d65ab638c36a5e2b90769bd3dd2f83ec248
Hindeu <whisper.hind@gmail.com>
2023-07-18 17:14:40 -0700

    Nested loops commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,8 @@
   * _isalpha - Checks for alphabetic character
   * @c : The character to be checked
   *
-  * Return : 1 if c is a letter or 0 for anything else
+  * Return: 1 if c is a letter, lowercase or uppercase
+  *         0 otherwise
   */
 int _isalpha(int c)
 {
