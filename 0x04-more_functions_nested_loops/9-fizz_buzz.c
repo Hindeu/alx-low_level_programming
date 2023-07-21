1182fdd80fafcf91a1580e2f962888a45b8bc55b
Hindeu <whisper.hind@gmail.com>
2023-07-20 17:27:02 -0700

    nested loops 2 commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - Prints a Fizz Buzz program
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (x % 3 == 0)
+		{
+			_putchar('F');
+                        _putchar('i');
+                        _putchar('z');
+                        _putchar('z');
+		}
+		else if (x % 5 == 0)
+		{
+			_putchar('B');
+                        _putchar('u');
+                        _putchar('z');
+                        _putchar('z');
+		}
+		else
+		{
+			_putchar("%d", x);
+		}
+
+		if (x != 100)
+		{
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
